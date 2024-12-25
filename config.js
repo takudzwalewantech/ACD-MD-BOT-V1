1660497e04793ebd55ab2a435b2b8ee880d5430e
takudzwalewantech <takudzwalewantech@gmail.com>
2024-12-25 14:57:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "add_your_session_id",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "SIVGUZzY#IdcDh98L8AYmAhFUes_wcGEiNitCRT4pGhWZwxMOCuo",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
-SUDO_NB: process.env.SUDO_NB || "94742398003",
+ALIVE_MSG: process.env.ALIVE_IMG || "*HEY lm Takudzwalewantech* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
+SUDO_NB: process.env.SUDO_NB || "263717467274",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
